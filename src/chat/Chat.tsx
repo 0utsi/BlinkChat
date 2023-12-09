91c22eba488859dd6df2fdd4161bf92df497951f
“Mateusz” <mateuszludyk@gmail.com>
2023-12-09 17:36:15 +0100

    localstorage

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,52 +1,52 @@
-import './Chat.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
 const socket = io("http://localhost:5174");
 
 export default function ChatRoom() {
-  socket.connect();
-  const [message, setMessage] = useState('');
-  const [messageReceived, setMessageReceived] = useState<Array<{ message: string; senderId: string }>>([]);
-  const [messages, setMessages] = useState<Array<{ message: string; senderId: string }>>([]);
+  // Room State
+  const [room, setRoom] = useState("");
+
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
 
   const sendMessage = () => {
-    const newMessage = { message, senderId: socket.id };
-    setMessages(prevMessages => [...prevMessages, newMessage]);
-    socket.emit('sendMessage', newMessage);
+    socket.emit("send_message", { message, room });
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      setMessages(prevMessages => [
-        ...prevMessages,
-        { message: data.message, senderId: data.senderId }
-      ]);
+      setMessages(prevMessages => [...prevMessages, data.message]);
     });
-  }, []);
+  }, [socket]);
 
   return (
-    <div className="chat">
-      <main>
-        {messages.map((msg, index) => (
-          <div key={index} className={msg.senderId === socket.id ? 'sent' : 'received'}>
-            <p>{msg.message}</p>
-            <p>Send by: {msg.senderId}</p>
-          </div>
-        ))}
-      </main>
-      <div className="manage">
-        <input
-          placeholder='Write smth nice'
-          type="text"
-          className="msg-input"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button className="sendBtn" onClick={sendMessage}>
-          <i className="far fa-paper-plane"></i>
-        </button>
-      </div>
+    <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Messages:</h1>
+      {messages.map((msg, index) => (
+        <p key={index}>{msg}</p>
+      ))}
     </div>
   );
 }
