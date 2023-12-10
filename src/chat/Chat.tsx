d03b47eb99d160c080fb92fecedb34600483c42a
“Mateusz” <mateuszludyk@gmail.com>
2023-12-10 14:39:23 +0100

    seperate mess, css classes to each one

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -15,7 +15,6 @@ export default function ChatRoom() {
 			socketId: socket.id,
 		};
 		setMessages(prevMessages => [...prevMessages, messageData]);
-		console.log(messageData)
 		socket.emit("send_message", messageData);
 	};
 
@@ -30,26 +29,27 @@ export default function ChatRoom() {
 
   return (
     <div className="chat">
-			<main>
+		<main>
 			{messages.map((msg, index) => (
-				<div key={index} className='message'>
+				<div key={index} className={msg.socketId === socket.id ? 'from' : 'to'}>
 				<p>{msg.message}</p>
-				<p>{msg.socketId}</p>
 				</div>
 			))}
-			</main>
-      <input
-        placeholder="Type.."
-		className='msg-input'
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-	<button className='sendBtn' onClick={sendMessage}>
-		<svg height="24" id="Layer_21" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-			<polygon points="3 12 8.61 14.992 17 8 9 17.455 9 21 12.164 16.887 18 20 21 3 3 12"/>
-		</svg>
-	</button>
+		</main>
+		<div className="manage">
+		<input
+			placeholder="Type.."
+			className='msg-input'
+			onChange={(event) => {
+			setMessage(event.target.value);
+			}}
+		/>
+		<button className='sendBtn' onClick={sendMessage}>
+			<svg height="50" id="Layer_21" viewBox="0 0 24 24" width="44" xmlns="http://www.w3.org/2000/svg">
+				<polygon fill="white" points="3 12 8.61 14.992 17 8 9 17.455 9 21 12.164 16.887 18 20 21 3 3 12"/>
+			</svg>
+		</button>
+		</div>
 
     </div>
   );
