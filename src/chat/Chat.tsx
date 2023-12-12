2a85b3082b046a3577c182653ad0783fe63372f7
“Mateusz” <mateuszludyk@gmail.com>
2023-12-12 17:35:59 +0100

    adding comp, styling

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -6,8 +6,9 @@ const socket = io("http://localhost:5174");
 export default function ChatRoom() {
 	socket.connect();
 	const [message, setMessage] = useState('')
-	const [messages, setMessages] = useState<Array<{message: string, socketId: string}>>([]);
-	const scrollRef = useRef(null)
+	const [messages, setMessages] = useState<{message: string, socketId: string}[]>([]);
+	const scrollRef = useRef<HTMLDivElement>(null);
+
 	const sendMessage = (e: { preventDefault: () => void; }) => {
 		e.preventDefault()
 		if(message.length === 0) return
@@ -24,15 +25,11 @@ export default function ChatRoom() {
 		socket.on("receive_message", (data) => {
 			setMessages(prevMessages => [...prevMessages, data]);
 		})
-
+		if(scrollRef.current)scrollRef.current.scrollIntoView()
 		return function off() {
 			socket.removeListener("receive_message");
 		};
 	});
-
-	useEffect(() => {
-		scrollRef.current.scrollIntoView()
-	},[messages])
 
   return (
     <div className="chat">
@@ -54,7 +51,7 @@ export default function ChatRoom() {
 			}}
 		/>
 		<button className='sendBtn' onClick={sendMessage}>
-			<svg height="35" id="Layer_21" viewBox="0 0 24 24" width="35" xmlns="http://www.w3.org/2000/svg">
+			<svg height="25" id="Layer_21" viewBox="0 0 24 24" width="25" xmlns="http://www.w3.org/2000/svg">
 				<polygon fill="white" points="3 12 8.61 14.992 17 8 9 17.455 9 21 12.164 16.887 18 20 21 3 3 12"/>
 			</svg>
 		</button>
---
+++ src/homepage/Welcome.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Welcome() {
+
+	return (
+		<div className="welcome">
+			<h1 className="title">BlinkChat</h1>
+		</div>
+	)
+}
