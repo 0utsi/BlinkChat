239c65e9ea82b2369e7b4048f2a8d608a9ea6147
“Mateusz” <mateuszludyk@gmail.com>
2023-12-14 14:40:01 +0100

    binding room, messages, refactor - working on

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,60 +1,74 @@
 import { useState, useEffect, useRef } from 'react';
-import io from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 import './Chat.css'
-const socket = io("http://localhost:5174");
+import { useParams } from "react-router-dom";
 
 export default function ChatRoom() {
-	socket.connect();
+	const { roomId } = useParams()
 	const [message, setMessage] = useState('')
-	const [messages, setMessages] = useState<{message: string, socketId: string}[]>([]);
+	const [messages, setMessages] = useState<{message: string, socketId: string | undefined}[]>([]);
+	const [socket, setSocket] = useState<Socket | undefined>(undefined)
 	const scrollRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const newSocket = io("http://localhost:5174", {
+			query: {
+				roomId: roomId
+			}
+		});
+		setSocket(newSocket.connect())
+		return () => {
+			newSocket.disconnect();
+		};
+	}, [roomId]);
 
 	const sendMessage = (e: { preventDefault: () => void; }) => {
 		e.preventDefault()
 		if(message.length === 0) return
 		const messageData = {
 			message,
-			socketId: socket.id,
+			socketId: socket?.id,
 		};
-		setMessages(prevMessages => [...prevMessages, messageData]);
-		socket.emit("send_message", messageData);
+		socket?.emit("send_message", messageData);
 		setMessage('')
 	};
 
 	useEffect(() => {
-		socket.on("receive_message", (data) => {
-			setMessages(prevMessages => [...prevMessages, data]);
-		})
-		if(scrollRef.current)scrollRef.current.scrollIntoView()
-		return function off() {
-			socket.removeListener("receive_message");
+		const receiveMessageHandler = (data: { message: string; socketId: string | undefined }) => {
+			setMessages((prevMessages) => [...prevMessages, data]);
 		};
-	});
+
+		socket?.on('receive_message', receiveMessageHandler);
+		if(scrollRef.current)scrollRef.current.scrollIntoView()
+		return () => {
+			socket?.off('receive_message', receiveMessageHandler);
+		};
+	}, [socket]);
 
   return (
     <div className="chat">
 		<main>
 			{messages.map((msg, index) => (
-				<div key={index} className={msg.socketId === socket.id ? 'from message' : 'to message'}>
+				<div key={index} className={msg.socketId === socket?.id ? 'from message' : 'to message'}>
 				<p>{msg.message}</p>
 				</div>
 			))}
 			<div ref={scrollRef} />
 		</main>
 		<form className="manage">
-		<input
-			placeholder="Type.."
-			className='msg-input'
-			value={message}
-			onChange={(event) => {
-			setMessage(event.target.value);
-			}}
-		/>
-		<button className='sendBtn' onClick={sendMessage}>
-			<svg height="25" id="Layer_21" viewBox="0 0 24 24" width="25" xmlns="http://www.w3.org/2000/svg">
-				<polygon fill="white" points="3 12 8.61 14.992 17 8 9 17.455 9 21 12.164 16.887 18 20 21 3 3 12"/>
-			</svg>
-		</button>
+			<input
+				placeholder="Type.."
+				className='msg-input'
+				value={message}
+				onChange={(event) => {
+				setMessage(event.target.value);
+				}}
+			/>
+			<button className='sendBtn' onClick={sendMessage}>
+				<svg height="25" id="Layer_21" viewBox="0 0 24 24" width="25" xmlns="http://www.w3.org/2000/svg">
+					<polygon fill="white" points="3 12 8.61 14.992 17 8 9 17.455 9 21 12.164 16.887 18 20 21 3 3 12"/>
+				</svg>
+			</button>
 		</form>
 
     </div>
--- src/homepage/Welcome.tsx
+++ src/homepage/Welcome.tsx
@@ -1,15 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './Welcome.css'
 
 export default function Welcome() {
 
+	const navigate = useNavigate();
+
+	const createRoom = () => {
+		const newRoomId = Math.floor(Math.random() * 1000)
+
+		navigate(`/room/${newRoomId}`);
+	}
+
 	return (
 		<div className="welcome">
 		<h1 className="title">BlinkChat</h1>
-				<button className="createBtn">Create a room</button>
+				<button className="createBtn" onClick={createRoom}>Create a room</button>
 				<div className="join">
 					<input type="number" placeholder='Room No..' className="roomId" />
 					<button className='roomBtn'>Join</button>
 				</div>
 		</div>
 	)
-}2
+}
