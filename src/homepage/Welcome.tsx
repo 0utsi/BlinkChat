75127cdddd3d8f5c7feec9d63bc7c732f8f851dc
“Mateusz” <mateuszludyk@gmail.com>
2023-12-18 16:53:36 +0100

    removing join

--- src/homepage/Welcome.tsx
+++ src/homepage/Welcome.tsx
@@ -1,9 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import './Welcome.css'
-import { useState } from 'react';
 
 export default function Welcome() {
-	const [roomId, setRoomId] = useState('')
 	const navigate = useNavigate();
 
 	const createRoom = () => {
@@ -12,19 +10,12 @@ export default function Welcome() {
 		navigate(`/room/${newRoomId}`);
 	}
 
-	const joinRoom = () => {
-		if(roomId  === '') return
-		navigate(`/room/${roomId}`);
-	}
 
 	return (
 		<div className="welcome">
 		<h1 className="title">BlinkChat</h1>
 				<button className="createBtn" onClick={createRoom}>Create a room</button>
-				<div className="join">
-					<input type="number" placeholder='Room No..' className="roomId" value={roomId} onChange={(e) => setRoomId(e.target.value)}/>
-					<button className='roomBtn' onClick={joinRoom}>Join</button>
-				</div>
+
 
 		</div>
 	)
